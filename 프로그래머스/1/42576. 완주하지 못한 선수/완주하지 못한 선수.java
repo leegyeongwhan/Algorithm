6d4da81cd5e06b27e7386056a1fecc64921d38ed
leegyeongwhan <53938366+leegyeongwhan@users.noreply.github.com>
2024-04-10 18:06:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 175.98 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+      public String solution(String[] participant, String[] completion) {
+            String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        //차이가 하나니까 participant맵에서 completion맵의 벨류값을 빼주면 남은 사람은 한명
+        for (String people : participant) {
+            if (!map.containsKey(people)) {
+                map.put(people, 1);
+            }else{
+                map.put(people, map.get(people)+1);
+            }
+        }
+
+        for (String people : completion) {
+            if (map.containsKey(people)) {
+                map.put(people, map.get(people)-1);
+            }
+        }
+
+          
+        for (Map.Entry<String, Integer> elem : map.entrySet()) {
+           if(elem.getValue()==1){
+                answer = elem.getKey();
+            }
+        }   
+    return answer;
+    }
+}
