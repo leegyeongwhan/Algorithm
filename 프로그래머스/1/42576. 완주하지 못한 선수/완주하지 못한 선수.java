c30dd09b45950effadca804cafa6e7c7c0e8c289
leegyeongwhan <53938366+leegyeongwhan@users.noreply.github.com>
2024-04-10 18:12:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 161.91 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,29 +3,44 @@ import java.util.Map;
 
 class Solution {
       public String solution(String[] participant, String[] completion) {
-            String answer = "";
-        Map<String, Integer> map = new HashMap<>();
-        //차이가 하나니까 participant맵에서 completion맵의 벨류값을 빼주면 남은 사람은 한명
-        for (String people : participant) {
-            if (!map.containsKey(people)) {
-                map.put(people, 1);
-            }else{
-                map.put(people, map.get(people)+1);
-            }
-        }
+//             String answer = "";
+//         Map<String, Integer> map = new HashMap<>();
+//         //차이가 하나니까 participant맵에서 completion맵의 벨류값을 빼주면 남은 사람은 한명
+//         for (String people : participant) {
+//             if (!map.containsKey(people)) {
+//                 map.put(people, 1);
+//             }else{
+//                 map.put(people, map.get(people)+1);
+//             }
+//         }
 
-        for (String people : completion) {
-            if (map.containsKey(people)) {
-                map.put(people, map.get(people)-1);
-            }
-        }
+//         for (String people : completion) {
+//             if (map.containsKey(people)) {
+//                 map.put(people, map.get(people)-1);
+//             }
+//         }
 
           
-        for (Map.Entry<String, Integer> elem : map.entrySet()) {
-           if(elem.getValue()==1){
-                answer = elem.getKey();
+//         for (Map.Entry<String, Integer> elem : map.entrySet()) {
+//            if(elem.getValue()==1){
+//                 answer = elem.getKey();
+//             }
+//         }   
+          
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) {
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
+        }
+        for (String player : completion){
+            hm.put(player, hm.get(player) - 1);
+        } 
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
-        }   
+        }
     return answer;
     }
 }
