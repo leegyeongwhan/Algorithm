c9167dfc64f06b87f98df7f3021cc0dc4ea3d93c
leegyeongwhan <53938366+leegyeongwhan@users.noreply.github.com>
2024-04-20 11:18:20 +0900

    [level 2] Title: 소수 찾기, Time: 25.99 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,51 @@
 import java.util.*;
 
 class Solution {
-        static int answer = 0;
-    static boolean[] visited = new boolean[7];
-    Set<Integer> set = new HashSet<>();
-
+    static int answer = 0;
+    static boolean[] visited;
+    static Set<Integer> set = new HashSet<>();
+    
     public int solution(String numbers) {
         int answer = 0;
-
-
-        dfs("", 0, numbers);
-
+        visited = new boolean[numbers.length()];
+        dfs(numbers, "", 0); //dfs를 돌며 나올수 있는 조합의 가짓수를 따져본다. 조합은 set에저장
+   
+        System.out.println(set);
+        
         for (Integer num : set) {
-            if (prime(num)) {
+            if(prime(num)){ //set을 돌며 소수인지 체크
                 answer++;
             }
         }
+        
         return answer;
     }
-
-    private boolean prime(int num) {
-           if (num == 1 ||  num == 0) {
-            return false;
-        }
-
-
-        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void dfs(String start, int depth, String numbers) {
-        if (depth > numbers.length()) {
+    
+    static void dfs(String numbers, String str, int depth){
+        if(depth > numbers.length()){
             return;
         }
-
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
+        
+        for(int i = 0; i < numbers.length(); i++){
+             if(!visited[i]) {
                 visited[i] = true;
-                set.add(Integer.valueOf(start + numbers.charAt(i)));
-                dfs(start + numbers.charAt(i), depth + 1, numbers);
+                 set.add(Integer.valueOf(str + numbers.charAt(i)));
+                dfs(numbers, str + numbers.charAt(i), depth+1);
                 visited[i] = false;
             }
         }
+    }
+    
+    static boolean prime(int num){
+        if(num < 2){
+            return false;
+        }
+        
+       for(int i = 2; i <= (int) Math.sqrt(num); i++){
+           if(num % i == 0){
+               return false;
+           }
+       }
+        return true;
     }
 }
